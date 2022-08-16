c4135bcc3d890f0cc6a9582d8bd7aa9be0dca182
Eunbin Kwon <kwoneunbini@gmail.com>
2022-08-16 12:25:31 +0900

    [chore] 프로젝트 파일 생성

---
+++ HanjaWordCreator/HanjaWordCreator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HanjaWordCreator
+//
+//  Created by 권은빈 on 2022/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HanjaWordCreator/HanjaWordCreator/HanjaWordCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanjaWordCreatorApp.swift
+//  HanjaWordCreator
+//
+//  Created by 권은빈 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct HanjaWordCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
